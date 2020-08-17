93c92f719b0e7052b32da0574e37bd12aed129df
Surekha <chillarasurekha11@gmail.com>
2020-08-17 12:04:29 -0400

    Updated the controller and service classes to autowire dependencies

--- src/main/java/com/kar/user/controller/UserController.java
+++ src/main/java/com/kar/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kar.user.controller;
 
 import com.kar.user.dto.User;
 import com.kar.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController("/")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping("/users")
--- src/main/java/com/kar/user/service/UserServiceImpl.java
+++ src/main/java/com/kar/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.kar.user.service;
 
 import com.kar.user.dto.User;
 import com.kar.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Override
